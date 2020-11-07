d6832258a26947aac441cce49b5896f749b1b144
Josue <josue.quilon-barrios@senecacollece.ca>
2020-11-07 03:05:29 -0500

    last changes

--- src/frontend/src/components/Posts/Outline.jsx
+++ src/frontend/src/components/Posts/Outline.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import useSWR from 'swr';
+import useSiteMetaData from '../../hooks/use-site-metadata';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -9,28 +10,37 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const Outline = ({ posts }) => {
+const ListElement = ({ postUrl }) => {
   const classes = useStyles();
-
+  const { data: post } = useSWR(postUrl, (url) => fetch(url).then((r) => r.json()));
+  if (!post) {
+    return <div className={classes.root}>Loading...</div>;
+  }
   return (
-    <ul>
-      {posts &&
-        posts[0].map((post) => {
-          console.log(post.id);
-          return (
-            <li key={post.id}>
-              <Typography className={classes.root} variant="span">
-                {post.id}
-              </Typography>
-            </li>
-          );
-        })}
-    </ul>
+    <li key={post.id} className={classes.root}>
+      <a className="" href={`#${post.id}`}>{`${post.feed.author} ${post.title}`}</a>
+    </li>
   );
+};
+
+const Outline = ({ posts }) => {
+  const { telescopeUrl } = useSiteMetaData();
+
+  if (!posts) return <div>Loading...</div>;
+
+  const postsUrl = posts.map((set) =>
+    set.map(({ id, url }) => <ListElement postUrl={`${telescopeUrl}${url}`} key={id} />)
+  );
+
+  return <ul>{postsUrl && postsUrl.map((post) => post)}</ul>;
 };
 
 Outline.propTypes = {
   posts: PropTypes.array,
+};
+
+ListElement.propTypes = {
+  postUrl: PropTypes.string,
 };
 
 export default Outline;
